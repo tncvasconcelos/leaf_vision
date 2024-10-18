18034d000c7168e5d4d4c70f363266fb0a2c0e0b
Thais Vasconcelos <tvasc@eeb-v32jt77k43.local>
2024-10-18 09:00:41 -0400

    adding stuff

---
+++ 06_post_processing.R
@@ -0,0 +1,108 @@
+setwd("/Users/tvasc/Desktop/leaf_computer_vision")
+#rm(list=ls())
+library(data.table)
+
+# function to make LMA from leaf area and petiole width
+# for woody dicots (based on Royer et al. 2007, r^2=0.55)
+make_LMA <- function(leaf_area, petiole_width) {
+  LMA = 3.07 + 0.382 * log(petiole_width^2 / leaf_area)
+  return(exp(LMA))
+}
+
+# Read Leaf Machine output
+all_measurements <- fread("data/LM2_MEASUREMENTS_CLEAN.csv")
+# Read petiole width data
+petiole_measurements <- fread("data/width_data.csv")
+# Merge datasets
+merged_dataset <- merge(all_measurements, petiole_measurements, by.x="component_name",by.y="filename")
+# remove NAs from area data
+merged_dataset <- subset(merged_dataset, !is.na(merged_dataset$area))
+
+merged_dataset$petiole_width <- NA
+for(i in 1:nrow(merged_dataset)) {
+  one_petiole <- merged_dataset$width_pixels[i]
+  if(merged_dataset$conversion_mean[i]!=0){
+    merged_dataset$petiole_width[i] <- one_petiole / merged_dataset$conversion_mean[i]
+  } else {
+    merged_dataset$petiole_width[i] <- one_petiole / merged_dataset$predicted_conversion_factor_cm[i]
+  }
+  cat(i, "\r")
+}
+
+write.csv(merged_dataset, file="data/merged_dataset.csv", row.names=F)
+
+###################
+###################
+###################
+merged_dataset <- fread("data/merged_dataset.csv")
+
+# some summary stats:
+min(merged_dataset$area)
+merged_dataset$filename[which.min(merged_dataset$area)]
+max(merged_dataset$area)
+merged_dataset$filename[which.max(merged_dataset$area)]
+
+pdf("results/leaf_area_dist.pdf")
+hist(log(merged_dataset$area), breaks=100, xlab="log(leaf area cm^2)", main="Leaf area distribution")
+dev.off()
+
+min(merged_dataset$petiole_width)
+merged_dataset$filename[which.min(merged_dataset$petiole_width)]
+max(merged_dataset$petiole_width)
+merged_dataset$filename[which.max(merged_dataset$petiole_width)]
+
+pdf("results/petiole_width_dist.pdf")
+hist(log(merged_dataset$petiole_width), breaks=100, xlab="log(petiole width cm)", main="Petiole width distribution")
+dev.off()
+
+pdf("results/cor_leaf_area_petiole_width.pdf")
+model <- lm(log(merged_dataset$area)~log(merged_dataset$petiole_width))
+summary(model)
+plot(log(merged_dataset$area)~log(merged_dataset$petiole_width), 
+     xlab="log(petiole width cm)", ylab="log(leaf area cm^2)")
+abline(model, col="red")
+dev.off()
+
+
+# Transform numbers into LMA
+merged_dataset$LMA <- NA
+for(i in 1:nrow(merged_dataset)) {
+  merged_dataset$LMA[i] <- make_LMA(merged_dataset$area[i], merged_dataset$petiole_width[i])
+  cat(i, "\r")
+}
+
+write.csv(merged_dataset, file="data/merged_dataset.csv", row.names=F)
+
+# Create a mean LMA for each specimen 
+
+lma_results <- data.frame(specimen=unique(merged_dataset$filename), mean_LMA=NA)
+for(i in 1:nrow(lma_results)) {
+  one_specimen <- subset(merged_dataset, merged_dataset$filename==lma_results$specimen[i])
+  lma_results$mean_LMA[i] <- mean(one_specimen$LMA)
+  cat(i, "\r")
+}
+
+pdf("results/LMA_dist.pdf")
+hist(log(lma_results$mean_LMA), breaks=100, xlab="log(LMA)", main="LMA distribution")
+dev.off()
+write.csv(lma_results, file="lma_results.csv", row.names=F)
+
+# merged_dataset <- subset(merged_dataset, !merged_dataset$petiole_width %in% tail(sort(merged_dataset$petiole_width), n=1000))
+# merged_dataset <- subset(merged_dataset, !merged_dataset$petiole_width %in% head(sort(merged_dataset$petiole_width), n=1000))
+# 
+# quantile(merged_dataset$petiole_width, probs = 0.75)
+
+
+### STILL WORKING HERE -- WILL MERGE METADATA TO GET LATS AND LONG ETC
+metadata <- list.files(path = "virtual_herbarium_NPleafPaper/metadata/", full.names = T)
+metadata <- lapply(metadata, read.csv)
+for(i in 1:length(metadata)) {
+  metadata[[i]] <- metadata[[i]][,c("gbifID","decimalLatitude","decimalLongitude")]
+  cat(i, "\r")
+}
+metadata <- do.call(rbind, metadata)
+
+grep(metadata$gbifID[1], lma_results$specimen[2])
+
+
+
---
+++ search2.R
@@ -0,0 +1,73 @@
+#---------------
+# Downloading herbarium specimens
+setwd("/Users/tvasc/Desktop/leaf_computer_vision")
+# rm(list=ls())
+library(mvh)
+
+species_to_sample <- read.csv("final_taxa_sample.csv")[,2]
+load("supporting_datasets/ref_table.Rsave")
+taxized_names <- unique(ref_table[,1][ref_table$newick_names %in% species_to_sample])
+
+#first_batch <- taxized_names[1:100]
+for(i in 1:length(taxized_names)) {
+  metadata <- search_specimen_metadata(taxon_name=taxized_names[i], limit=100, hasCoordinate=T)
+  # Removing duplicates based on coordinates
+  metadata <- subset(metadata, !duplicated(paste0(metadata$decimalLatitude,"_",metadata$decimalLongitude)))
+  if(nrow(metadata)>5) {
+    # Adding preference scores
+    metadata$preference_score <- 0
+    metadata$preference_score <-  metadata$preference_score + metadata$year
+    # Favor eventDate not between September and May for decimalLatitude > 30
+    metadata$preference_score <- metadata$preference_score + ifelse(metadata$decimalLatitude > 30 & (metadata$month >= 9 | metadata$month <= 5), 0, 1000)
+    # Favor eventDate between September and May for decimalLatitude < 30
+    metadata$preference_score <- metadata$preference_score + ifelse(metadata$decimalLatitude < 30 & (metadata$month >= 9 & metadata$month <= 5), 1000, 0)
+    # A lot of these are field photos, so let's reduce their score
+    metadata$preference_score <- metadata$preference_score - ifelse(grepl("urn:catalog:MO:Tropicos", metadata$identifier), 1000, 0)
+    if(!is.null(metadata$institutionCode)) {
+      # NYBG scores higher
+      metadata$preference_score <- metadata$preference_score + ifelse(metadata$institutionCode%in%"NY", 1000, 0)
+      # BRI,NSW and RBGE score lower (low res)
+      metadata$preference_score <- metadata$preference_score - ifelse(metadata$institutionCode%in%"NSW", 1000, 0)
+      metadata$preference_score <- metadata$preference_score - ifelse(metadata$institutionCode%in%"BRI", 1000, 0)
+      metadata$preference_score <- metadata$preference_score - ifelse(metadata$institutionCode%in%"RBGE", 1000, 0)
+      metadata$preference_score <- metadata$preference_score - ifelse(metadata$institutionCode%in%"P", 1000, 0)
+    }
+    # Sort by preference score (higher is better)
+    metadata <- metadata[order(metadata$preference_score,decreasing=T), ]
+    # Sample up to 5 rows from the sorted data.frame
+    metadata <- head(metadata, 20)
+  }
+  write.csv(metadata, file=paste0("virtual_herbarium_NPleafPaper/metadata/",metadata$scientificName[1]," metadata.csv"))
+  
+  n_images_in_folder <- 0
+    for(j in 1:nrow(metadata)) {  
+      try(download_specimen_images(metadata[j,],
+                                   dir_name="virtual_herbarium_NPleafPaper/final_dataset/",
+                                   result_file_name=paste0("virtual_herbarium_NPleafPaper/result_files/",metadata$scientificName[j],metadata$gbifID[j])))
+      
+      # check quality
+      file_name <- paste0("virtual_herbarium_NPleafPaper/final_dataset/",gsub(" ","_",paste(metadata$species[j],metadata$gbifID[j])),".jpeg")
+      try_img <- try(magick::image_read(file_name), silent = TRUE)
+      if(!inherits(try_img, "try-error")) {  # Check if resizing succeeded
+        current_width <- magick::image_info(try_img)$width
+        current_height <- magick::image_info(try_img)$height
+        # Calculate the current megapixels
+        current_megapixels <- (current_width * current_height) / 1e6
+        if(length(current_megapixels) == 0) {
+          file.remove(file_name)
+          next
+        }
+        if(current_megapixels < 3) {
+          file.remove(file_name)
+        }
+      } else {
+        file.remove(file_name)
+      }
+      n_images_in_folder <- length(list.files("virtual_herbarium_NPleafPaper/final_dataset/",gsub(" ","_",paste(metadata$species[1]))))
+      if(n_images_in_folder>=5){
+        break
+      }
+    }   
+  cat(i, "\r")
+}
+
