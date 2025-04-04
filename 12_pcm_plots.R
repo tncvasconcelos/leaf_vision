623a8858785dc107f9e14a9d97f5053fa1f5cc6a
jboyko <jamesboyko21@gmail.com>
2025-04-03 21:09:10 -0400

    working se again

--- 12_pcm_plots.R
+++ 12_pcm_plots.R
@@ -28,28 +28,28 @@ phy$node.label <- NULL
 
 #--------------------------
 # BIO1
-tmp <- data.frame(aggregate(cbind(dat$LMA, dat$bio_1), list(dat$filename, dat$genus_species), 
+tmp <- data.frame(aggregate(cbind(dat$LMA, dat$bio_1), list(dat$filename, dat$genus_species),
                             function(x) c(mean(x), mean(log(x)))))
 bio1_dat <- data.frame(sp = tmp$Group.2, bio_1 = tmp$V2[,1], lma = log(exp(tmp$V1[,2])*100))
-model <- extended.pgls(
-  lma ~ bio1,
-  phy = phy,
-  Cov = NULL,
-  species = "sp",
-  delta = 0.001,
-  gamma = c("sample", "equal"),
-  iter = 999,
-  seed = NULL,
-  int.first = FALSE,
-  turbo = FALSE,
-  Parallel = FALSE,
-  verbose = FALSE,
-  data = bio1_dat,
-  print.progress = TRUE
-)
-save(model, file="bio1_extd_extd_model.Rsave")
+# model <- extended.pgls(
+#   lma ~ bio_1,
+#   phy = phy,
+#   Cov = NULL,
+#   species = "sp",
+#   delta = 0.001,
+#   gamma = c("sample", "equal"),
+#   iter = 999,
+#   seed = NULL,
+#   int.first = FALSE,
+#   turbo = FALSE,
+#   Parallel = FALSE,
+#   verbose = FALSE,
+#   data = bio1_dat,
+#   print.progress = TRUE
+# )
+# save(model, file="bio1_extd_model.Rsave")
 
-load(file="bio1_extd_extd_model.Rsave")
+load(file="bio1_extd_model.Rsave")
 lrtest_bio1 <- anova(model)
 coef_model <- coef(model)
 p_value <- lrtest_bio1$table$`Pr(>F)`[1]
@@ -69,7 +69,7 @@ lma_bio_1_plot <- ggplot(bio1_dat, aes(x = bio_1, y = lma)) +
     color = "black", linetype = "dashed", linewidth = 1
   ) +
   labs(
-    x = "Mean Diurnal Range",
+    x = "Mean Annual Temperature",
     y = expression("log LMA " (g/m^2))
   ) +
   # annotate(
@@ -87,28 +87,28 @@ lma_bio_1_plot <- ggplot(bio1_dat, aes(x = bio_1, y = lma)) +
 
 #--------------------------
 # BIO12
-tmp <- data.frame(aggregate(cbind(dat$LMA, dat$bio_12), list(dat$filename, dat$genus_species), 
+tmp <- data.frame(aggregate(cbind(dat$LMA, dat$bio_12), list(dat$filename, dat$genus_species),
                             function(x) c(mean(x), mean(log(x)))))
 bio12_dat <- data.frame(sp = tmp$Group.2, bio_12 = tmp$V2[,1], lma = log(exp(tmp$V1[,2])*100))
-model <- extended.pgls(
-  lma ~ bio_12,
-  phy = phy,
-  Cov = NULL,
-  species = "sp",
-  delta = 0.001,
-  gamma = c("sample", "equal"),
-  iter = 999,
-  seed = NULL,
-  int.first = FALSE,
-  turbo = FALSE,
-  Parallel = FALSE,
-  verbose = FALSE,
-  data = bio12_dat,
-  print.progress = TRUE
-)
-save(model, file="bio12_extd_ives_model.Rsave")
+# model <- extended.pgls(
+#   lma ~ bio_12,
+#   phy = phy,
+#   Cov = NULL,
+#   species = "sp",
+#   delta = 0.001,
+#   gamma = c("sample", "equal"),
+#   iter = 999,
+#   seed = NULL,
+#   int.first = FALSE,
+#   turbo = FALSE,
+#   Parallel = FALSE,
+#   verbose = FALSE,
+#   data = bio12_dat,
+#   print.progress = TRUE
+# )
+# save(model, file="bio12_extd_model.Rsave")
 
-load(file="bio12_extd_ives_model.Rsave")
+load(file="bio12_extd_model.Rsave")
 lrtest_bio12 <- anova(model)
 coef_model <- coef(model)
 p_value <- lrtest_bio12$table$`Pr(>F)`[1]
@@ -141,28 +141,28 @@ lma_bio_12_plot <- ggplot(bio12_dat, aes(x = bio_12, y = lma)) +
 
 #--------------------------
 # Solar radiation
-tmp <- data.frame(aggregate(cbind(dat$LMA, dat$srad), list(dat$filename, dat$genus_species), 
-                            function(x) c(mean(x), mean(log(x)))))
-srad_dat <- data.frame(sp = tmp$Group.2, srad = tmp$V2[,1], lma = log(exp(tmp$V1[,2])*100))
-model <- extended.pgls(
-  lma ~ srad,
-  phy = phy,
-  Cov = NULL,
-  species = "sp",
-  delta = 0.001,
-  gamma = c("sample", "equal"),
-  iter = 999,
-  seed = NULL,
-  int.first = FALSE,
-  turbo = FALSE,
-  Parallel = FALSE,
-  verbose = FALSE,
-  data = srad_dat,
-  print.progress = TRUE
-)
-save(model, file="srad_extd_ives_model.Rsave")
+# tmp <- data.frame(aggregate(cbind(dat$LMA, dat$srad), list(dat$filename, dat$genus_species), 
+#                             function(x) c(mean(x), mean(log(x)))))
+# srad_dat <- data.frame(sp = tmp$Group.2, srad = tmp$V2[,1], lma = log(exp(tmp$V1[,2])*100))
+# model <- extended.pgls(
+#   lma ~ srad,
+#   phy = phy,
+#   Cov = NULL,
+#   species = "sp",
+#   delta = 0.001,
+#   gamma = c("sample", "equal"),
+#   iter = 999,
+#   seed = NULL,
+#   int.first = FALSE,
+#   turbo = FALSE,
+#   Parallel = FALSE,
+#   verbose = FALSE,
+#   data = srad_dat,
+#   print.progress = TRUE
+# )
+# save(model, file="srad_extd_model.Rsave")
 
-load(file="srad_extd_ives_model.Rsave")
+load(file="srad_extd_model.Rsave")
 lrtest_srad <- anova(model)
 coef_model <- coef(model)
 p_value <- lrtest_srad$table$`Pr(>F)`[1]
@@ -195,28 +195,28 @@ lma_srad_plot <- ggplot(srad_dat, aes(x = srad, y = lma)) +
 
 #--------------------------
 # BIO4
-tmp <- data.frame(aggregate(cbind(dat$LMA, dat$bio_4), list(dat$filename, dat$genus_species), 
-                            function(x) c(mean(x), mean(log(x)))))
-bio4_dat <- data.frame(sp = tmp$Group.2, bio_4 = tmp$V2[,1], lma = log(exp(tmp$V1[,2])*100))
-model <- extended.pgls(
-  lma ~ bio_4,
-  phy = phy,
-  Cov = NULL,
-  species = "sp",
-  delta = 0.001,
-  gamma = c("sample", "equal"),
-  iter = 999,
-  seed = NULL,
-  int.first = FALSE,
-  turbo = FALSE,
-  Parallel = FALSE,
-  verbose = FALSE,
-  data = bio4_dat,
-  print.progress = TRUE
-)
-save(model, file="bio4_extd_ives_model.Rsave")
+# tmp <- data.frame(aggregate(cbind(dat$LMA, dat$bio_4), list(dat$filename, dat$genus_species), 
+#                             function(x) c(mean(x), mean(log(x)))))
+# bio4_dat <- data.frame(sp = tmp$Group.2, bio_4 = tmp$V2[,1], lma = log(exp(tmp$V1[,2])*100))
+# model <- extended.pgls(
+#   lma ~ bio_4,
+#   phy = phy,
+#   Cov = NULL,
+#   species = "sp",
+#   delta = 0.001,
+#   gamma = c("sample", "equal"),
+#   iter = 999,
+#   seed = NULL,
+#   int.first = FALSE,
+#   turbo = FALSE,
+#   Parallel = FALSE,
+#   verbose = FALSE,
+#   data = bio4_dat,
+#   print.progress = TRUE
+# )
+# save(model, file="bio4_extd_model.Rsave")
 
-load(file="bio4_extd_ives_model.Rsave")
+load(file="bio4_extd_model.Rsave")
 lrtest_bio4 <- anova(model)
 coef_model <- coef(model)
 p_value <- lrtest_bio4$table$`Pr(>F)`[1]
@@ -249,28 +249,28 @@ lma_bio_4_plot <- ggplot(bio4_dat, aes(x = bio_4, y = lma)) +
 
 #--------------------------
 # BIO15
-tmp <- data.frame(aggregate(cbind(dat$LMA, dat$bio_15), list(dat$filename, dat$genus_species), 
-                            function(x) c(mean(x), mean(log(x)))))
-bio15_dat <- data.frame(sp = tmp$Group.2, bio_15 = tmp$V2[,1], lma = log(exp(tmp$V1[,2])*100))
-model <- extended.pgls(
-  lma ~ bio_15,
-  phy = phy,
-  Cov = NULL,
-  species = "sp",
-  delta = 0.001,
-  gamma = c("sample", "equal"),
-  iter = 999,
-  seed = NULL,
-  int.first = FALSE,
-  turbo = FALSE,
-  Parallel = FALSE,
-  verbose = FALSE,
-  data = bio15_dat,
-  print.progress = TRUE
-)
-save(model, file="bio15_extd_ives_model.Rsave")
+# tmp <- data.frame(aggregate(cbind(dat$LMA, dat$bio_15), list(dat$filename, dat$genus_species), 
+#                             function(x) c(mean(x), mean(log(x)))))
+# bio15_dat <- data.frame(sp = tmp$Group.2, bio_15 = tmp$V2[,1], lma = log(exp(tmp$V1[,2])*100))
+# model <- extended.pgls(
+#   lma ~ bio_15,
+#   phy = phy,
+#   Cov = NULL,
+#   species = "sp",
+#   delta = 0.001,
+#   gamma = c("sample", "equal"),
+#   iter = 999,
+#   seed = NULL,
+#   int.first = FALSE,
+#   turbo = FALSE,
+#   Parallel = FALSE,
+#   verbose = FALSE,
+#   data = bio15_dat,
+#   print.progress = TRUE
+# )
+# save(model, file="bio15_extd_model.Rsave")
 
-load(file="bio15_extd_ives_model.Rsave")
+load(file="bio15_extd_model.Rsave")
 lrtest_bio15 <- anova(model)
 coef_model <- coef(model)
 p_value <- lrtest_bio15$table$`Pr(>F)`[1]
@@ -303,28 +303,28 @@ lma_bio_15_plot <- ggplot(bio15_dat, aes(x = bio_15, y = lma)) +
 
 #--------------------------
 # Wind speed
-tmp <- data.frame(aggregate(cbind(dat$LMA, dat$wind), list(dat$filename, dat$genus_species), 
-                            function(x) c(mean(x), mean(log(x)))))
-wind_dat <- data.frame(sp = tmp$Group.2, wind = tmp$V2[,1], lma = log(exp(tmp$V1[,2])*100))
-model <- extended.pgls(
-  lma ~ wind,
-  phy = phy,
-  Cov = NULL,
-  species = "sp",
-  delta = 0.001,
-  gamma = c("sample", "equal"),
-  iter = 999,
-  seed = NULL,
-  int.first = FALSE,
-  turbo = FALSE,
-  Parallel = FALSE,
-  verbose = FALSE,
-  data = wind_dat,
-  print.progress = TRUE
-)
-save(model, file="wind_extd_ives_model.Rsave")
+# tmp <- data.frame(aggregate(cbind(dat$LMA, dat$wind), list(dat$filename, dat$genus_species), 
+#                             function(x) c(mean(x), mean(log(x)))))
+# wind_dat <- data.frame(sp = tmp$Group.2, wind = tmp$V2[,1], lma = log(exp(tmp$V1[,2])*100))
+# model <- extended.pgls(
+#   lma ~ wind,
+#   phy = phy,
+#   Cov = NULL,
+#   species = "sp",
+#   delta = 0.001,
+#   gamma = c("sample", "equal"),
+#   iter = 999,
+#   seed = NULL,
+#   int.first = FALSE,
+#   turbo = FALSE,
+#   Parallel = FALSE,
+#   verbose = FALSE,
+#   data = wind_dat,
+#   print.progress = TRUE
+# )
+# save(model, file="wind_extd_model.Rsave")
 
-load(file="wind_extd_ives_model.Rsave")
+load(file="wind_extd_model.Rsave")
 lrtest_wind <- anova(model)
 coef_model <- coef(model)
 p_value <- lrtest_wind$table$`Pr(>F)`[1]
@@ -357,28 +357,28 @@ lma_wind_plot <- ggplot(wind_dat, aes(x = wind, y = lma)) +
 
 #--------------------------
 # AI (Aridity Index)
-tmp <- data.frame(aggregate(cbind(dat$LMA, dat$ai), list(dat$filename, dat$genus_species), 
-                            function(x) c(mean(x), mean(log(x)))))
-ai_dat <- data.frame(sp = tmp$Group.2, ai = tmp$V2[,1], lma = log(exp(tmp$V1[,2])*100))
-model <- extended.pgls(
-  lma ~ ai,
-  phy = phy,
-  Cov = NULL,
-  species = "sp",
-  delta = 0.001,
-  gamma = c("sample", "equal"),
-  iter = 999,
-  seed = NULL,
-  int.first = FALSE,
-  turbo = FALSE,
-  Parallel = FALSE,
-  verbose = FALSE,
-  data = ai_dat,
-  print.progress = TRUE
-)
-save(model, file="ai_extd_ives_model.Rsave")
+# tmp <- data.frame(aggregate(cbind(dat$LMA, dat$ai), list(dat$filename, dat$genus_species), 
+#                             function(x) c(mean(x), mean(log(x)))))
+# ai_dat <- data.frame(sp = tmp$Group.2, ai = tmp$V2[,1], lma = log(exp(tmp$V1[,2])*100))
+# model <- extended.pgls(
+#   lma ~ ai,
+#   phy = phy,
+#   Cov = NULL,
+#   species = "sp",
+#   delta = 0.001,
+#   gamma = c("sample", "equal"),
+#   iter = 999,
+#   seed = NULL,
+#   int.first = FALSE,
+#   turbo = FALSE,
+#   Parallel = FALSE,
+#   verbose = FALSE,
+#   data = ai_dat,
+#   print.progress = TRUE
+# )
+# save(model, file="ai_extd_model.Rsave")
 
-load(file="ai_extd_ives_model.Rsave")
+load(file="ai_extd_model.Rsave")
 lrtest_ai <- anova(model)
 coef_model <- coef(model)
 p_value <- lrtest_ai$table$`Pr(>F)`[1]
